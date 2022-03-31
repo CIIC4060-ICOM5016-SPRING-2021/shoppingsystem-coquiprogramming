e5c834ac4d24194366fad132f2791035eaee713d
joseanrodz1 <Josean.rodriguez1@upr.edu>
2022-03-31 14:52:41 -0400

    Credentials added to pg

--- config/pg.py
+++ config/pg.py
@@ -1,6 +1,6 @@
 pg_config = {
-'user' : '',
-'password' : '',
-'dbname': '',
-'dbport': ''
+'user' : 'vklmqqnbrudglf',
+'password' : '01e0c61352e174e36b0c72ea1032855e14cd323bd06399de5d15abab93a04928',
+'dbname': 'd7okpnllqijpgi',
+'dbport': '5432'
 }
