a274e8c253baf3699f2f9ecbf80043137af40f4a
joseanrodz1 <Josean.rodriguez1@upr.edu>
2022-04-11 17:36:10 -0400

    orderinfo now references price bought from orderhas

--- dao/orders.py
+++ dao/orders.py
@@ -19,7 +19,7 @@ class OrderDAO:
         return result
 
     def getOrderInfoById(self, order_id):
-        query = "select part_name,part_price, partquantity, partquantity * part_price as totalPartPrice from (orderhas left join parts p on p.part_id = " \
+        query = "select part_name,price_bought, partquantity, partquantity * price_bought as totalPartPrice from (orderhas left join parts p on p.part_id = " \
                 "orderhas.part_id) where order_id = %s "
         cursor = self.conn.cursor()
         cursor.execute(query, (order_id,))
